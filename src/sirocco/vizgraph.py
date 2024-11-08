593778fdc5638d771f9c266e111cd66240730ee6
Matthieu <matthieu.leclair@c2sm.ethz.ch>
2024-11-08 15:45:32 +0100

    fix: hatch fmt

--- src/sirocco/vizgraph.py
+++ src/sirocco/vizgraph.py
@@ -14,37 +14,37 @@ from sirocco.core import Workflow
 
 def hsv_to_hex(h: float, s: float, v: float) -> str:
     r, g, b = hsv_to_rgb(h, s, v)
-    return "#{:02x}{:02x}{:02x}".format(*map(round,(255*r, 255*g, 255*b)))
+    return "#{:02x}{:02x}{:02x}".format(*map(round, (255 * r, 255 * g, 255 * b)))
 
-def node_colors(h: float) -> dict[str: str]:
-    fill = hsv_to_hex(h/365, 0.15, 1)
-    border = hsv_to_hex(h/365, 1, 0.20)
-    font = hsv_to_hex(h/365, 1, 0.15)
-    return {'fillcolor': fill, 'color': border , 'fontcolor': font}
+
+def node_colors(h: float) -> dict[str:str]:
+    fill = hsv_to_hex(h / 365, 0.15, 1)
+    border = hsv_to_hex(h / 365, 1, 0.20)
+    font = hsv_to_hex(h / 365, 1, 0.15)
+    return {"fillcolor": fill, "color": border, "fontcolor": font}
 
 
 class VizGraph:
     """Class for visualizing a Sirocco workflow"""
 
-    node_base_kw: ClassVar[dict[str: Any]] = {'style': 'filled', 'fontname': 'Fira Sans', 'fontsize': 14, 'penwidth': 2}
-    edge_base_kw: ClassVar[dict[str: Any]] = {'color': '#77767B', 'penwidth': 1.5}
-    data_node_base_kw: ClassVar[dict[str: Any]] = node_base_kw | {'shape': 'ellipse'}
+    node_base_kw: ClassVar[dict[str:Any]] = {"style": "filled", "fontname": "Fira Sans", "fontsize": 14, "penwidth": 2}
+    edge_base_kw: ClassVar[dict[str:Any]] = {"color": "#77767B", "penwidth": 1.5}
+    data_node_base_kw: ClassVar[dict[str:Any]] = node_base_kw | {"shape": "ellipse"}
 
-    data_av_node_kw: ClassVar[dict[str: Any]] = data_node_base_kw | node_colors(116)
-    data_gen_node_kw: ClassVar[dict[str: Any]] = data_node_base_kw | node_colors(214)
-    task_node_kw: ClassVar[dict[str: Any]] = node_base_kw | {'shape': 'box'} | node_colors(354)
-    io_edge_kw: ClassVar[dict[str: Any]] = edge_base_kw
-    wait_on_edge_kw: ClassVar[dict[str: Any]] = edge_base_kw | {'style': 'dashed'}
-    cluster_kw: ClassVar[dict[str: Any]] = {'bgcolor': '#F6F5F4', 'color': None, 'fontsize': 16}
+    data_av_node_kw: ClassVar[dict[str:Any]] = data_node_base_kw | node_colors(116)
+    data_gen_node_kw: ClassVar[dict[str:Any]] = data_node_base_kw | node_colors(214)
+    task_node_kw: ClassVar[dict[str:Any]] = node_base_kw | {"shape": "box"} | node_colors(354)
+    io_edge_kw: ClassVar[dict[str:Any]] = edge_base_kw
+    wait_on_edge_kw: ClassVar[dict[str:Any]] = edge_base_kw | {"style": "dashed"}
+    cluster_kw: ClassVar[dict[str:Any]] = {"bgcolor": "#F6F5F4", "color": None, "fontsize": 16}
 
-    def __init__(self, name:str, cycles: Store, data: Store) -> None:
-
+    def __init__(self, name: str, cycles: Store, data: Store) -> None:
         self.name = name
-        self.agraph = AGraph(name=name, fontname='Fira Sans', newrank=True)
+        self.agraph = AGraph(name=name, fontname="Fira Sans", newrank=True)
         for data_node in data.values():
             gv_kw = self.data_av_node_kw if data_node.available else self.data_gen_node_kw
             tooltip = data_node.name if data_node.date is None else f"{data_node.name}\n {data_node.date}"
-            self.agraph.add_node(data_node, tooltip=tooltip,label=data_node.name, **gv_kw)
+            self.agraph.add_node(data_node, tooltip=tooltip, label=data_node.name, **gv_kw)
 
         k = 1
         for cycle in cycles.values():
@@ -65,37 +65,39 @@ class VizGraph:
             cluster_label = cycle.name
             if cycle.date is not None:
                 cluster_label += "\n" + cycle.date.isoformat()
-            self.agraph.add_subgraph(cluster_nodes,
-                                     name=f'cluster_{cycle.name}_{k}',
-                                     clusterrank='global',
-                                     label=cluster_label,
-                                     tooltip=cluster_label,
-                                     **self.cluster_kw)
+            self.agraph.add_subgraph(
+                cluster_nodes,
+                name=f"cluster_{cycle.name}_{k}",
+                clusterrank="global",
+                label=cluster_label,
+                tooltip=cluster_label,
+                **self.cluster_kw,
+            )
             k += 1
 
     def draw(self, **kwargs):
         # draw graphviz dot graph to svg file
-        self.agraph.layout(prog='dot')
-        file_path = Path(f'./{self.name}.svg')
-        self.agraph.draw(path=file_path, format='svg', **kwargs)
+        self.agraph.layout(prog="dot")
+        file_path = Path(f"./{self.name}.svg")
+        self.agraph.draw(path=file_path, format="svg", **kwargs)
 
         # Add interactive capabilities to the svg graph thanks to
         # https://github.com/BartBrood/dynamic-SVG-from-Graphviz
 
         # Parse svg
-        svg = etree.parse(file_path)
+        svg = etree.parse(file_path)  # noqa: S320 this svg is safe as generated internaly
         svg_root = svg.getroot()
         # Add 'onload' tag
-        svg_root.set('onload', 'addInteractivity(evt)')
+        svg_root.set("onload", "addInteractivity(evt)")
         # Add css style for interactivity
         this_dir = Path(__file__).parent
-        style_file_path = this_dir / 'svg-interactive-style.css'
-        node = etree.Element('style')
+        style_file_path = this_dir / "svg-interactive-style.css"
+        node = etree.Element("style")
         node.text = style_file_path.read_text()
         svg_root.append(node)
         # Add scripts
-        js_file_path = this_dir / 'svg-interactive-script.js'
-        node = etree.Element('script')
+        js_file_path = this_dir / "svg-interactive-script.js"
+        node = etree.Element("script")
         node.text = etree.CDATA(js_file_path.read_text())
         svg_root.append(node)
 
--- tests/test_wc_workflow.py
+++ tests/test_wc_workflow.py
@@ -11,7 +11,7 @@ config_test_files = ["tests/files/configs/test_config_small.yml", "tests/files/c
 @pytest.mark.parametrize("config_file", config_test_files)
 def test_parse_config_file(config_file):
     config_path = Path(config_file)
-    reference_str = (config_path.parent/".."/"data"/config_path.name).with_suffix('.txt').read_text()
+    reference_str = (config_path.parent / ".." / "data" / config_path.name).with_suffix(".txt").read_text()
     test_str = str(Workflow.from_yaml(config_file))
     if test_str != reference_str:
         new_path = Path(config_file).with_suffix(".new.txt")
@@ -24,8 +24,9 @@ def test_parse_config_file(config_file):
 @pytest.mark.parametrize("config_file", config_test_files)
 def test_serialize_workflow(config_file):
     config_path = Path(config_file)
-    reference_path = (config_path.parent/".."/"data"/config_path.name).with_suffix('.txt')
+    reference_path = (config_path.parent / ".." / "data" / config_path.name).with_suffix(".txt")
     reference_path.write_text(str(Workflow.from_yaml(config_file)))
+
 
 @pytest.mark.parametrize("config_file", config_test_files)
 def test_vizgraph(config_file):
