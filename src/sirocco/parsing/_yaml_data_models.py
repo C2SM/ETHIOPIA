8c7c30b11b7c7871a8166b44fbd5e0673adb6d90
Julian Geiger <julian.geiger@psi.ch>
2024-12-16 14:53:47 +0100

    Add `_CliArgsBaseModel` keyword validation

--- src/sirocco/parsing/_yaml_data_models.py
+++ src/sirocco/parsing/_yaml_data_models.py
@@ -92,11 +92,18 @@ class _CliArgsBaseModel(BaseModel):
     flags: str | list[str] | None = None
     source_file: str | list[str] | None = None
 
-    # def validate_source_file: ...
-    #   path existing
-
     # def validate_keyword_args: ...
     #   starts with `-` or `--`
+
+    # TODO: Should we validate here, or allow users to pass it without the hyphen(s), and prepend them automatically?
+    @field_validator("keyword", mode="before")
+    def validate_keyword_args(cls, value):
+        """Validate that keyword arguments start with '-' or '--'."""
+        if value is not None:
+            invalid_keys = [key for key in value.keys() if not key.startswith(("-", "--"))]
+            if invalid_keys:
+                raise ValueError(f"Invalid keyword arguments: {', '.join(invalid_keys)}")
+        return value
 
 
 class TargetNodesBaseModel(_NamedBaseModel):
