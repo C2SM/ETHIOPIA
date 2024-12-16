6f76d129c7402484b5777bf7b8094f419f2a8457
Julian Geiger <julian.geiger@psi.ch>
2024-12-16 12:22:32 +0100

    Update `yml` files and `ShellTask` to use `_CliArgBaseModel`

--- src/sirocco/core/_tasks/shell_task.py
+++ src/sirocco/core/_tasks/shell_task.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
+from pathlib import Path
 from dataclasses import dataclass
 from typing import ClassVar
 
 from sirocco.core.graph_items import Task
+from sirocco.parsing._yaml_data_models import _CliArgBaseModel
 
 
 @dataclass
@@ -11,6 +13,5 @@ class ShellTask(Task):
     plugin: ClassVar[str] = "shell"
 
     command: str | None = None
-    command_option: str | None = None
-    input_arg_options: dict[str, str] | None = None
-    src: str | None = None
+    src: str | Path | None = None
+    cli_arguments: _CliArgBaseModel | None = None
