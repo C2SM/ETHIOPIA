9a9c09302a690f16a895dff5828e0a0b80731ae3
Julian Geiger <julian.geiger@psi.ch>
2024-12-17 14:37:51 +0100

    Fix svgs being created in main directory. (#67)
    Create `svg` files in subdirectory under `tests/files/svgs` instead of top-level directory, and ignore them via `.gitignore`, while syncing the `svgs` directory.

--- tests/test_wc_workflow.py
+++ tests/test_wc_workflow.py
@@ -20,31 +20,29 @@ config_test_files = [
 
 
 @pytest.fixture(params=config_test_files)
-def config_case(request):
+def config_paths(request):
     config_path = Path(request.param)
-    return (
-        config_path,
-        (config_path.parent.parent / "data" / config_path.name).with_suffix(".txt"),
-    )
+    return {
+        "yml": config_path,
+        "txt": (config_path.parent.parent / "data" / config_path.name).with_suffix(".txt"),
+        "svg": (config_path.parent.parent / "svgs" / config_path.name).with_suffix(".svg"),
+    }
 
 
-def test_parse_config_file(config_case, pprinter):
-    config_path, reference_path = config_case
-    reference_str = reference_path.read_text()
-    test_str = pprinter.format(Workflow.from_yaml(config_path))
+def test_parse_config_file(config_paths, pprinter):
+    reference_str = config_paths["txt"].read_text()
+    test_str = pprinter.format(Workflow.from_yaml(config_paths["yml"]))
     if test_str != reference_str:
-        new_path = Path(reference_path).with_suffix(".new.txt")
+        new_path = Path(config_paths["txt"]).with_suffix(".new.txt")
         new_path.write_text(test_str)
         msg = f"Workflow graph doesn't match serialized data. New graph string dumped to {new_path}."
         raise ValueError(msg)
 
 
 @pytest.mark.skip(reason="don't run it each time, uncomment to regenerate serilaized data")
-def test_serialize_workflow(config_case, pprinter):
-    config_path, reference_path = config_case
-    reference_path.write_text(pprinter.format(Workflow.from_yaml(config_path)))
+def test_serialize_workflow(config_paths, pprinter):
+    config_paths["txt"].write_text(pprinter.format(Workflow.from_yaml(config_paths["yml"])))
 
 
-def test_vizgraph(config_case):
-    config_path, _ = config_case
-    VizGraph.from_yaml(config_path).draw()
+def test_vizgraph(config_paths):
+    VizGraph.from_yaml(config_paths["yml"]).draw(file_path=config_paths["svg"])
